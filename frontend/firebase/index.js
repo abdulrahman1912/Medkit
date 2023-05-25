d03cd644b81c9aa7279bfafde86771df462dcabb
abdulrahman1912 <rahmanadeiza@gmail.com>
2023-05-25 19:02:29 +0100

    Firebase Integration

--- frontend/firebase/index.js
+++ frontend/firebase/index.js
@@ -5,7 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
- 
+    apiKey: "AIzaSyDCfwDaDPpILxpRPxUmnpJK_tMvkWITjMQ",
+    authDomain: "medkit-ff62e.firebaseapp.com",
+    projectId: "medkit-ff62e",
+    storageBucket: "medkit-ff62e.appspot.com",
+    messagingSenderId: "999743092929",
+    appId: "1:999743092929:web:1477080ee9f1859c3b7c40",
+    measurementId: "G-LB63C3PYG7"
 };
 
 // Initialize Firebase
