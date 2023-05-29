cbf9e2cf8d937dcd9772fb0ad77efccdcc2a4664
raji2004 <kingraj1344@gmail.com>
2023-05-29 16:37:25 +0100

    done

--- admin/src/components/Nav/Nav.jsx
+++ admin/src/components/Nav/Nav.jsx
@@ -8,15 +8,20 @@ import customer from '../../assets/customer.svg'
 import product from '../../assets/product.svg'
 import Transaction from '../../assets/Transaction.svg'
 import prescribtion from '../../assets/prescribtion.svg'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate,useLocation } from 'react-router-dom'
 
 export const Nav = () => {
     const navigate = useNavigate()
+    const location = useLocation()
+
+
+    console.log(location.pathname)
+    
   return (
     <div className={Style.Container}>
       <img src={logonav} alt="" />
       <div className={Style.middle} >
-        <Row img={dashboard} text="Dashboard" onClick={()=>{navigate('/dashboard/main')}}/>
+        <Row img={dashboard} text="Dashboard" onClick={()=>{navigate('/dashboard/main')}} />
         <Row img={customer} text="Customer" onClick={()=>{navigate('/dashboard/customer')}}/> 
         <Row img={product} text="Product" onClick={()=>{navigate('/dashboard/product')}}/>  
         <Row img={Transaction} text="Transaction" onClick={()=>{navigate('/dashboard/Transaction')}}/>
--- admin/src/components/RowComponent/Row.jsx
+++ admin/src/components/RowComponent/Row.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Style from './Row.module.css'
 
 
-export const Row = ({img,text,onClick,style}) => {
+export const Row = ({img,text,onClick,style,focus}) => {
+  
   return (
-    <div className={Style.Cotainer} style={style} onClick={onClick}>
+    <div className={focus?Style.Cotainer1:Style.Cotainer} style={style} onClick={onClick}>
     <img src={img} alt="" />
     <div className={Style.Text}>{text}</div>
     </div>
