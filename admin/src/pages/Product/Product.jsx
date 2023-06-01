e1dc15d6068d873a18ecebdd5aa793f401e94dc5
raji <kingraj1344@gmail.com>
2023-06-01 12:40:32 +0100

    done

--- admin/src/pages/Product/Product.jsx
+++ admin/src/pages/Product/Product.jsx
@@ -13,31 +13,95 @@ import vitamins from '../../assets/vitamins.svg'
 
 
 export const Product = () => {
-   
+
     return (
         <div className={Style.main}>
             <SpaceBetween
                 style={{ fontSize: 30, fontWeight: '800' }}
                 text1={'Categories'}
-              
+
             />
 
             <div className={Style.row}>
-                <Card img={antibacterial} text={'Anti Bacterial'} number={28} color={'#DC2028'} imgSize={50}  fontSize={30} focus />
-                <Card img={antidepressent} text={'Anti Depressent'} number={28} color={'#FC657E'}  imgSize={50}  fontSize={30} focus />
-                <Card img={antifungi} text={'Anti Fungi'} number={28} color={'#DC2028'} imgSize={50}  fontSize={30} focus />
-                <Card img={antihypertensive} text={'Anti Hypertensive'} number={28} color={'#DA4531'} imgSize={50}  fontSize={27} focus />
+                <Card
+                    img={antibacterial}
+                    text={'Anti Bacterial'}
+                    number={28}
+                    color={'#DC2028'}
+                    imgSize={50}
+                    fontSize={30}
+                    focus
+                />
+                <Card
+                    img={antidepressent}
+                    text={'Anti Depressent'}
+                    number={28}
+                    color={'#FC657E'}
+                    imgSize={50}
+                    fontSize={30}
+                    focus
+                />
+                <Card
+                    img={antifungi}
+                    text={'Anti Fungi'}
+                    number={28}
+                    color={'#DC2028'}
+                    imgSize={50}
+                    fontSize={30}
+                    focus
+                />
+                <Card
+                    img={antihypertensive}
+                    text={'Anti Hypertensive'}
+                    number={28}
+                    color={'#DA4531'}
+                    imgSize={50}
+                    fontSize={27}
+                    focus
+                />
             </div>
-            
+
             <div className={Style.row}>
-                <Card img={antimaleria} text={'Anti Maleria'} number={28} color={'#FC657E'} imgSize={50}  fontSize={30} focus />
-                <Card img={tablet} text={'Tablets'} number={28} color={'#82CA04'} imgSize={50}  fontSize={30} focus />
-                <Card img={syrup} text={'Syrup'} number={28} color={'#5686E1'} imgSize={50}  fontSize={30} focus />
-                <Card img={vitamins} text={'Vitamins'} number={28} color={'#FECE2C'} imgSize={50}  fontSize={30} focus />
+                <Card
+                    img={antimaleria}
+                    text={'Anti Maleria'}
+                    number={28}
+                    color={'#FC657E'}
+                    imgSize={50}
+                    fontSize={30}
+                    focus
+                />
+                <Card
+                    img={tablet}
+                    text={'Tablets'}
+                    number={28}
+                    color={'#82CA04'}
+                    imgSize={50}
+                    fontSize={30}
+                    focus
+                />
+                <Card
+                    img={syrup}
+                    text={'Syrup'}
+                    number={28}
+                    color={'#5686E1'}
+                    imgSize={50}
+                    fontSize={30}
+                    focus
+                />
+                <Card
+                    img={vitamins}
+                    text={'Vitamins'}
+                    number={28}
+                    color={'#FECE2C'}
+                    imgSize={50}
+                    fontSize={30}
+                    focus
+                />
             </div>
 
 
-    
+
         </div>
     )
 }
